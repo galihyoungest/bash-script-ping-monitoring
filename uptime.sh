4c7336c29229fab80fac5e49879a74641c835e0c
galihyoungest <galihyoungest@gmail.com>
2023-01-15 00:16:58 +0700

    fix logging wording

--- uptime.sh
+++ uptime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 status="initial"
-HOST="192.168.1.1" # host to check
+HOST="192.168.111.1" # host to check
 delay="1" # in second
 LOGS="log.uptime"
 
@@ -23,7 +23,7 @@ logging(){
 	finish=$(date +%s)
 	time=$(($finish-$start))
 	calctime=$(echo $(($time / 3600)) hours $(($time / 60)) minutes $(($time % 60)) seconds )
-	echo "$(date "+%F %H:%M:%S"): $curstatus after $calctime $status" | tee -a $LOGS
+	echo "$(date "+%F %H:%M:%S"): $HOST is $curstatus after $calctime $status" | tee -a $LOGS
 	start=$(date +%s)
 };
 
@@ -31,7 +31,7 @@ start=$(date +%s)
 while true; do
 	check
 	if [ $status == "initial" ]; then
-		echo "monitoring $HOST started at $(date "+%F %H:%M:%S") with status $curstatus" | tee -a $LOGS
+		echo "$(date "+%F %H:%M:%S"): $HOST monitoring started with current status $curstatus" | tee -a $LOGS
 	elif [ $curstatus != $status ]; then
 		logging
 	fi
